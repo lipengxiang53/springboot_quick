d3a367ccd66b37e5f1b8271d50863ff5077c4624
lipengxiang53 <583118405@qq.com>
2019-11-11 22:25:23 +0800

    SpringBoot项目

--- src/main/java/com/ztesoft/controller/QuickController.java
+++ src/main/java/com/ztesoft/controller/QuickController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * @Auther Lee53
+ * @Date 2019-11-11 22:23
+ */
 @Controller
 public class QuickController {
     @ResponseBody
